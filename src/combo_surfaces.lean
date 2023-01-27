232498421932c16a9413682a1ab3bfa5a3881d27
Maxwell Thum <maxwell.thum2718@gmail.com>
2023-01-26 21:33:54 -0800

    Misc. changes; old code removed

--- src/combo_surfaces.lean
+++ src/combo_surfaces.lean
@@ -19,16 +19,13 @@ and for all `σ ∈ S`,
 
 universes u v
 
-variables {A : Type*}
-
-def fin_abstr_simpl_complex {A : Type*} (K : finset (finset A)) :=
-  ∀ σ ∈ K, ∀ τ ⊆ σ, τ ∈ K
+variables (A : Type*)
 
 /-- Based off of `analysis.convex.simplicial_complex.basic` and 
 `https://ncatlab.org/nlab/show/simplicial+complex`. -/
-@[ext]
-structure abstract_simplicial_complex :=
---(vertices : set A) -- maybe we don't start with the vertices, we start with the simplices and let vertices be 1-simplices
+--@[ext]
+class abstract_simplicial_complex := -- making this a class again?
+--(vertices : set A) -- maybe we don't start with the vertices, we start with the simplices and let vertices be 0-simplices
 (simplices : set (finset A))
 --(simpl_subset_𝒫vert : ∀ σ ∈ simplices, ↑σ ⊆ vertices)
 (not_empty_mem : ∅ ∉ simplices)
@@ -37,18 +34,23 @@ structure abstract_simplicial_complex :=
 
 namespace abstract_simplicial_complex
 
-example (K : finset (finset ℕ)) (hK : K = { ∅, {1}, {2}, {1, 2}, {3}}) : fin_abstr_simpl_complex K :=
-by { rw [hK, fin_abstr_simpl_complex], dec_trivial }
+instance example_1 : abstract_simplicial_complex ℕ :=
+{ simplices := { {1}, {2}, {1, 2}, {3}},
+  not_empty_mem := dec_trivial,
+  down_closed := by simp ; dec_trivial,
+}
 
-example (K : finset (finset ℕ)) (hK : K = { ∅, {1}, {2}, {3}, {4}, {5}, {1,2}, {1,3}, {2,3}, {1,4}, {2,4}, {1,2,3}, {1,2,4}, {4,5}}) : fin_abstr_simpl_complex K :=
-by { rw [hK, fin_abstr_simpl_complex], dec_trivial }
+/- Getting some error about class instance depth
+instance example_2 : abstract_simplicial_complex ℕ :=
+{ simplices := {{1}, {2}, {3}, {4}, {5}, {1,2}, {1,3}, {2,3}, {1,4}, {2,4}, {1,2,3}, {1,2,4}, {4,5}},
+  not_empty_mem := dec_trivial,
+  down_closed := by simp ; dec_trivial,
+}
+-/
 
-def fin_abstr_simplex {A : Type*} (k : ℕ) (σ : finset A) (K : finset (finset A)) {hK : fin_abstr_simpl_complex K} :=
-  σ ∈ K ∧ (finset.card σ = k)
+def p_simplices {A : Type*} (K : abstract_simplicial_complex A) (p : ℕ) := 
+  { σ ∈ K.simplices | finset.card σ = p+1 }
 
-def p_simplices (K : @abstract_simplicial_complex A) (p : ℕ) := 
-  { σ ∈ K.simplices | finset.card σ = p }
-
-def vertices (K : @abstract_simplicial_complex A) := p_simplices K 1
+def vertices (K : abstract_simplicial_complex A) := p_simplices K 0
 
 end abstract_simplicial_complex
